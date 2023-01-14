ba584ec575734b96a90b18ecebaf4a341f8b2d8c
ZkyZ77 <96150776+ZkyZ77@users.noreply.github.com>
2023-01-14 22:10:24 +0300

    Update mc.sh

--- .github/workflows/mc.sh
+++ .github/workflows/mc.sh
@@ -86,5 +86,5 @@ touch logs/latest.log
 # Start minecraft
 #PATH=$PWD/jre/bin:$PATH
 echo "Running server..."
-java -Xmx512M -Xms512M -jar paper-1.19.3-368.jar nogui
+java -Xmx512M -Xms512M -jar server.jar nogui
 echo "Exit code $?"
