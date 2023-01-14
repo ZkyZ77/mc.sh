1f9533406408afe2b8bfed7607b3c3e74a7f7516
ZkyZ77 <96150776+ZkyZ77@users.noreply.github.com>
2023-01-14 21:41:05 +0300

    Update mc.sh

--- .github/workflows/mc.sh
+++ .github/workflows/mc.sh
@@ -74,6 +74,7 @@ mkdir -p ./logs
 touch ./logs/temp # avoid "no such file or directory"
 rm ./logs/*
 orig_server_ip=`curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'`
+trimmed_server_ip=`echo $orig_server_ip`
 server_ip="${trimmed_server_ip:-$orig_server_ip}"
 echo "Server IP is: $server_ip"
 echo "Server running on: $server_ip" > $root/ip.txt
